e069610e7cf30a8f5abba2949746db07859487da
Vishal Saini <131337601+Vishal-The-Nonpareil@users.noreply.github.com>
2023-08-14 18:43:03 +0530

    Update Test4.java

--- 5.1Questions/Test4.java
+++ 5.1Questions/Test4.java
@@ -2,7 +2,7 @@ public class Test4 {
     public static void main(String[] args) {
         int x = 9, y = 12;
         int a = 2, b = 4, c = 6;
-        int exp = 4 / 3 * (x + 34) + 9 * (a + b + c) + (3 + y * (2 + a)) / (a + b * y);
+        int exp = 4 / 3 * (x + 34) + 9 * (a + b * c) + (3 + y * (2 + a)) / (a + b * y);
         System.out.println(exp);
     }
 
