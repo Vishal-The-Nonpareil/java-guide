50f7279e1dd5cf66585e46ccb65ddd7e5a7a7903
Vishal Saini <131337601+Vishal-The-Nonpareil@users.noreply.github.com>
2023-08-20 18:53:55 +0530

    Add files via upload

---
+++ 6.0.Condition-Statement/Switch.java
@@ -0,0 +1,20 @@
+public class Switch {
+    public static void main(String[] args) {
+
+        int number = 4;
+        switch (number) {
+            case 1:
+                System.out.println("Dosa");
+                break;
+            case 2:
+                System.out.println("Samosa");
+                break;
+            case 3:
+                System.out.println("Shakes");
+                break;
+            default:
+                System.out.println("We realise we are dreaming");
+        }
+    }
+
+}
