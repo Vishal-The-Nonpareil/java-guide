61aca46947d705a189879760f0d1761d229e1d55
Vishal Saini <131337601+Vishal-The-Nonpareil@users.noreply.github.com>
2023-08-23 21:30:22 +0530

    Add files via upload

---
+++ 8.0.Pattern-basics/HalfPyramidPattern.java
@@ -0,0 +1,11 @@
+public class HalfPyramidPattern {
+    public static void main(String[] args) {
+        int n = 7;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
