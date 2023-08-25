9cc3ab40d1644794d44739f2c72351530f0b89f0
Vishal Saini <131337601+Vishal-The-Nonpareil@users.noreply.github.com>
2023-08-25 15:59:07 +0530

    Update HalfPyramidPattern.java

--- 8.0.Pattern-basics/HalfPyramidPattern.java
+++ 8.0.Pattern-basics/HalfPyramidPattern.java
@@ -1,9 +1,10 @@
 public class HalfPyramidPattern {
     public static void main(String[] args) {
-        int n = 7;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(j);
+        int n = 4;
+
+        for (int line = 1; line <= n; line++) {
+            for (int number = 1; number <= line; number++) {
+                System.out.print(number);
             }
             System.out.println();
         }
