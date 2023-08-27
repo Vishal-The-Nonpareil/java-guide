e22b9739c61b9f95addec2791172e2724b93484a
Vishal Saini <131337601+Vishal-The-Nonpareil@users.noreply.github.com>
2023-08-27 23:54:09 +0530

    Add files via upload

---
+++ 9.0.Functionds-And-Methods/FunctionCode.java
@@ -0,0 +1,14 @@
+public class FunctionCode {
+
+    public static void printHelloWorld() {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        return;
+    }
+
+    public static void main(String[] args) {
+        printHelloWorld();
+    }
+
+}
