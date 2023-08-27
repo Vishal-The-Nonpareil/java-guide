fbb4d920115e11770dbe72dad8d9f94b622e6f5f
Vishal Saini <131337601+Vishal-The-Nonpareil@users.noreply.github.com>
2023-08-27 20:15:03 +0530

    Update MethodWithParameter.java

--- 9.0.Functionds-And-Methods/MethodWithParameter.java
+++ 9.0.Functionds-And-Methods/MethodWithParameter.java
@@ -2,17 +2,19 @@ import java.util.Scanner;
 
 public class MethodWithParameter {
 
-    public static void calSum() {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int sum = a + b;
+    public static void calSum(int num1, int num2) {
+
+        int sum = num1 + num2;
         System.out.println("Sum is : " + sum);
-        sc.close();
     }
 
     public static void main(String[] args) {
-        calSum();
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+
+        calSum(a, b);
     }
 
 }
