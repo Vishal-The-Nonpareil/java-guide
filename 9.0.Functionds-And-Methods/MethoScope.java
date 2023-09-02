b4d69762317460061994d81628a36accebfde698
Vishal Saini <131337601+Vishal-The-Nonpareil@users.noreply.github.com>
2023-09-02 14:06:32 +0530

    Add files via upload

---
+++ 9.0.Functionds-And-Methods/MethoScope.java
@@ -0,0 +1,14 @@
+public class MethoScope {
+    // public static void printS() {
+    // int s = 42;
+    // }
+
+    public static void main(String[] args) {
+        // System.out.println(s);
+
+        int s = 45;
+        System.out.println(s);
+
+    }
+
+}
