b3cd30536eebc82fe8ca1235ce43db6dc2bd8607
Vishal Saini <131337601+Vishal-The-Nonpareil@users.noreply.github.com>
2023-09-02 22:41:20 +0530

    Update BlockScope.java

--- 9.0.Functionds-And-Methods/BlockScope.java
+++ 9.0.Functionds-And-Methods/BlockScope.java
@@ -1,10 +1,14 @@
 public class BlockScope {
     public static void main(String[] args) {
+
+        int p = 76; // Function Scope
         {
             int s = 56;
-            System.out.println(s);
+            System.out.println(s); // Block Scope
+            System.out.println(p);
         }
         // System.out.println(s);
+        System.out.println(p);
     }
 
 }
