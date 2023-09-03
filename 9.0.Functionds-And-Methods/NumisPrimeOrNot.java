1010d9f4d2b0451503c87a52b83aaa9f3ac77aa3
Vishal Saini <131337601+Vishal-The-Nonpareil@users.noreply.github.com>
2023-09-03 12:42:48 +0530

    Add files via upload

---
+++ 9.0.Functionds-And-Methods/NumisPrimeOrNot.java
@@ -0,0 +1,19 @@
+public class NumisPrimeOrNot {
+
+    public static boolean isprime(int n) {
+        boolean isPrime = true;
+        for (int i = 2; i <= n - 1; i++) {
+            if (n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        return isPrime;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isprime(5));
+
+    }
+
+}
